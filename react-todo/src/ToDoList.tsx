deef4f9466eabecd739ec965dcc06bdb52844613
jinyoung <sm099@naver.com>
2022-07-16 23:38:47 +0900

    refactor : todo form change

--- react-todo/src/ToDoList.tsx
+++ react-todo/src/ToDoList.tsx
@@ -1,75 +1,25 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-interface IForm {
-    email: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    password1: string;
-  }
-  
-  function ToDoList() {
-    const {register, handleSubmit, formState: { errors }} = useForm<IForm>({
-      defaultValues: {
-        email: "@naver.com",
-      },
-    });
-    const onValid = (data: any) => {
-      console.log(data);
-    };
-    return (
-      <div>
-        <form
-          style={{ display: "flex", flexDirection: "column" }}
-          onSubmit={handleSubmit(onValid)}
-        >
-          <input
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                message: "Only naver.com emails allowed",
-              },
-            })}
-            placeholder="Email"
-          />
-          <span>{errors?.email?.message}</span>
-          <input
-            {...register("firstName", { required: "write here" })}
-            placeholder="First Name"
-          />
-          <span>{errors?.firstName?.message}</span>
-          <input
-            {...register("lastName", { required: "write here" })}
-            placeholder="Last Name"
-          />
-          <span>{errors?.lastName?.message}</span>
-          <input
-            {...register("username", { required: "write here", minLength: 10 })}
-            placeholder="Username"
-          />
-          <span>{errors?.username?.message}</span>
-          <input
-            {...register("password", { required: "write here", minLength: 5 })}
-            placeholder="Password"
-          />
-          <span>{errors?.password?.message}</span>
-          <input
-            {...register("password1", {
-              required: "Password is required",
-              minLength: {
-                value: 5,
-                message: "Your password is too short.",
-              },
-            })}
-            placeholder="Password1"
-          />
-          <span>{errors?.password1?.message}</span>
-          <button>Add</button>
-        </form>
-      </div>
-    );
-  }
-  export default ToDoList;
+interface IValidSubmit {
+  toDo : string,
+}
+
+function ToDoList() {
+  const {register, handleSubmit, setValue} = useForm<IValidSubmit>();
+  const validSubmit = (data:IValidSubmit) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
+  };
+  return (
+    <form onSubmit={handleSubmit(validSubmit)}>
+      <input {...register("toDo", {
+        required: "please add to-Do"
+      })}
+        placeholder="add To do"
+      />
+      <button>add</button>
+    </form>
+  );
+}
+export default ToDoList;
